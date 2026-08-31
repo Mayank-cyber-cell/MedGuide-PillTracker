4f14eaad54f8ee6e204ba0d423f9bbfcbe386cda
Mayank-Shah <147410698+Mayank-cyber-cell@users.noreply.github.com>
2026-08-31 17:12:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
+ 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   const isProduction = mode === 'production';
